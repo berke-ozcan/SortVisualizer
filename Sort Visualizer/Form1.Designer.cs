5e34438ab6a4c4cddcf9df87a6bfd922d63e7f5a
berke-ozcan <142122706+berke-ozcan@users.noreply.github.com>
2024-06-17 22:06:05 +0300

    xd

--- Sort Visualizer/Form1.Designer.cs
+++ Sort Visualizer/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            btn_rng = new Button();
+            btn_reset = new Button();
+            cbx_noi = new ComboBox();
+            btn_numberOfItems = new Button();
+            btn_delayChange = new Button();
+            cbx_delay = new ComboBox();
+            tmr_delay = new System.Windows.Forms.Timer(components);
+            btn_Sort = new Button();
+            cbx_algs = new ComboBox();
+            SuspendLayout();
+            // 
+            // btn_rng
+            // 
+            btn_rng.Location = new Point(1136, 375);
+            btn_rng.Name = "btn_rng";
+            btn_rng.Size = new Size(230, 90);
+            btn_rng.TabIndex = 0;
+            btn_rng.Text = "Randomize numbers";
+            btn_rng.UseVisualStyleBackColor = true;
+            btn_rng.Click += btn_rng_Click;
+            // 
+            // btn_reset
+            // 
+            btn_reset.Location = new Point(1136, 12);
+            btn_reset.Name = "btn_reset";
+            btn_reset.Size = new Size(230, 90);
+            btn_reset.TabIndex = 1;
+            btn_reset.Text = "Reset";
+            btn_reset.UseVisualStyleBackColor = true;
+            btn_reset.Click += btn_reset_Click;
+            // 
+            // cbx_noi
+            // 
+            cbx_noi.FormattingEnabled = true;
+            cbx_noi.Items.AddRange(new object[] { "8", "16", "32", "64", "128", "256", "512" });
+            cbx_noi.Location = new Point(1136, 133);
+            cbx_noi.Name = "cbx_noi";
+            cbx_noi.Size = new Size(230, 33);
+            cbx_noi.TabIndex = 2;
+            cbx_noi.Text = "64";
+            // 
+            // btn_numberOfItems
+            // 
+            btn_numberOfItems.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_numberOfItems.Location = new Point(1136, 172);
+            btn_numberOfItems.Name = "btn_numberOfItems";
+            btn_numberOfItems.Size = new Size(230, 51);
+            btn_numberOfItems.TabIndex = 3;
+            btn_numberOfItems.Text = "Change the number of items";
+            btn_numberOfItems.UseVisualStyleBackColor = true;
+            btn_numberOfItems.Click += btn_numberOfItems_Click;
+            // 
+            // btn_delayChange
+            // 
+            btn_delayChange.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_delayChange.Location = new Point(1136, 293);
+            btn_delayChange.Name = "btn_delayChange";
+            btn_delayChange.Size = new Size(230, 51);
+            btn_delayChange.TabIndex = 5;
+            btn_delayChange.Text = "Change the delay btwn actions\r\n";
+            btn_delayChange.UseVisualStyleBackColor = true;
+            btn_delayChange.Click += btn_delayChange_Click;
+            // 
+            // cbx_delay
+            // 
+            cbx_delay.FormattingEnabled = true;
+            cbx_delay.Items.AddRange(new object[] { "5", "10", "20", "50", "100", "200", "500", "1000" });
+            cbx_delay.Location = new Point(1136, 254);
+            cbx_delay.Name = "cbx_delay";
+            cbx_delay.Size = new Size(230, 33);
+            cbx_delay.TabIndex = 4;
+            cbx_delay.Text = "50";
+            // 
+            // tmr_delay
+            // 
+            tmr_delay.Interval = 50;
+            // 
+            // btn_Sort
+            // 
+            btn_Sort.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Sort.Location = new Point(1136, 535);
+            btn_Sort.Name = "btn_Sort";
+            btn_Sort.Size = new Size(230, 51);
+            btn_Sort.TabIndex = 8;
+            btn_Sort.Text = "Sort";
+            btn_Sort.UseVisualStyleBackColor = true;
+            btn_Sort.Click += btn_Sort_Click;
+            // 
+            // cbx_algs
+            // 
+            cbx_algs.FormattingEnabled = true;
+            cbx_algs.Items.AddRange(new object[] { "Bubble Sort" });
+            cbx_algs.Location = new Point(1136, 496);
+            cbx_algs.Name = "cbx_algs";
+            cbx_algs.Size = new Size(230, 33);
+            cbx_algs.TabIndex = 7;
+            cbx_algs.Text = "Bubble Sort";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1378, 1099);
+            Controls.Add(btn_Sort);
+            Controls.Add(cbx_algs);
+            Controls.Add(btn_delayChange);
+            Controls.Add(cbx_delay);
+            Controls.Add(btn_numberOfItems);
+            Controls.Add(cbx_noi);
+            Controls.Add(btn_reset);
+            Controls.Add(btn_rng);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btn_rng;
+        private Button btn_reset;
+        private ComboBox cbx_noi;
+        private Button btn_numberOfItems;
+        private Button btn_delayChange;
+        private ComboBox cbx_delay;
+        private System.Windows.Forms.Timer tmr_delay;
+        private Button btn_Sort;
+        private ComboBox cbx_algs;
     }
 }
--- Sort Visualizer/Form1.cs
+++ Sort Visualizer/Form1.cs
@@ -1,10 +1,163 @@
+﻿using System.Drawing;
+using System.Security.Policy;
+
 namespace Sort_Visualizer
 {
     public partial class Form1 : Form
     {
+        int[] numbers = new int[512];
+        Graphics g;
+        int numberOfItems;
+        int delay;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            resetNumbers(numberOfItems);
+            PrepareScene(numberOfItems);
+        }
+
+        void PrepareScene(int size)
+        {
+            //Arka Plan
+            g = this.CreateGraphics();
+            g.FillRectangle(new SolidBrush(Color.Black), new Rectangle(0, 0, 1101, 1100));
+
+            //Sayıları göster
+            FillNumbers(size);
+        }
+
+        void resetNumbers(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = i + 1;
+            }
+        }
+
+        void FillNumbers(int size)
+        {
+            for (int n = 0; n < size; n++)
+            {
+                g.FillRectangle(new SolidBrush(Color.White), new Rectangle(39 + (n * 1024 / size) + 256 / size, 1100 - (numbers[n] * 1024 / size) - 38, 512 / size, numbers[n] * 1024 / size));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numberOfItems = 64;
+            delay = 50;
+            Thread.Sleep(5);
+            PrepareScene(64);
+        }
+
+        private void btn_numberOfItems_Click(object sender, EventArgs e)
+        {
+            numberOfItems = Int32.Parse(cbx_noi.Text);
+            resetNumbers(numberOfItems);
+            PrepareScene(numberOfItems);
+        }
+
+        void swapTwo(int index1, int index2)
+        {
+            //Şartlar sağlanıyor mu
+            if (numberOfItems <= index1 || numberOfItems <= index2)
+                return;
+
+            //Highlight (ve ses, eklenecek)
+            g.FillRectangle(new SolidBrush(Color.Red), new Rectangle(39 + (index1 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index1] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index1] * 1024 / numberOfItems));
+            g.FillRectangle(new SolidBrush(Color.Red), new Rectangle(39 + (index2 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index2] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index2] * 1024 / numberOfItems));
+
+            //Delay
+            Thread.Sleep(delay);
+
+            //Delete the highlights
+            g.FillRectangle(new SolidBrush(Color.Black), new Rectangle(39 + (index1 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index1] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index1] * 1024 / numberOfItems));
+            g.FillRectangle(new SolidBrush(Color.Black), new Rectangle(39 + (index2 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index2] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index2] * 1024 / numberOfItems));
+
+            //Swap
+            int temp = numbers[index1];
+            numbers[index1] = numbers[index2];
+            numbers[index2] = temp;
+
+            //Refill the rectangles
+            g.FillRectangle(new SolidBrush(Color.White), new Rectangle(39 + (index1 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index1] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index1] * 1024 / numberOfItems));
+            g.FillRectangle(new SolidBrush(Color.White), new Rectangle(39 + (index2 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index2] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index2] * 1024 / numberOfItems));
+
+        }
+
+        int compareTwo(int index1, int index2) //\\ 1 - Index1 büyük //\\ 2 - Index2 büyük //\\ 0 - Diğer //\\
+        {
+            //Şartlar sağlanıyor mu
+            if (numberOfItems <= index1 || numberOfItems <= index2)
+                return 0;
+
+            //Highlight (ve ses, eklenecek)
+            g.FillRectangle(new SolidBrush(Color.Green), new Rectangle(39 + (index1 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index1] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index1] * 1024 / numberOfItems));
+            g.FillRectangle(new SolidBrush(Color.Green), new Rectangle(39 + (index2 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index2] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index2] * 1024 / numberOfItems));
+
+            //Delay
+            Thread.Sleep(delay);
+
+            //Delete green highlights
+            g.FillRectangle(new SolidBrush(Color.White), new Rectangle(39 + (index1 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index1] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index1] * 1024 / numberOfItems));
+            g.FillRectangle(new SolidBrush(Color.White), new Rectangle(39 + (index2 * 1024 / numberOfItems) + 256 / numberOfItems, 1100 - (numbers[index2] * 1024 / numberOfItems) - 38, 512 / numberOfItems, numbers[index2] * 1024 / numberOfItems));
+
+            //Comparison
+            if (numbers[index1] > numbers[index2]) return 1;
+            return 2;
+        }
+
+        private void btn_delayChange_Click(object sender, EventArgs e)
+        {
+            delay = Int32.Parse(cbx_delay.Text);
+        }
+
+        private void btn_Sort_Click(object sender, EventArgs e)
+        {
+            if (cbx_algs.Text == "Bubble Sort") bubbleSort();
+        }
+
+        void bubbleSort()
+        {
+            int i, j;
+            bool swapped;
+            for (i = 0; i < numberOfItems - 1; i++)
+            {
+                swapped = false;
+                for (j = 0; j < numberOfItems - i - 1; j++)
+                {
+                    if (compareTwo(j, j + 1) == 1)
+                    {
+                        swapTwo(j, j + 1);
+                        swapped = true;
+                    }
+                }
+
+                if (swapped == false)
+                    break;
+            }
+        }
+
+        private void btn_rng_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int temp = delay;
+            delay = 5;
+            for (int i = 0; i <= numberOfItems*2; i++)
+            {
+                int r1 = random.Next(0,numberOfItems-1);
+                int r2 = random.Next(0,numberOfItems-1);
+                if (r1 != r2)
+                {
+                    swapTwo(r1, r2);
+                }
+            }
+            delay = temp;
         }
     }
 }
